a7067115b4c246e4394926495af780ee17833d40
RobsonLMDS <98341530+Robsonlmds@users.noreply.github.com>
2024-10-25 13:27:14 -0300

    Adicionar arquivos de projeto.

---
+++ HospitalSystem/Controllers/HospitalControlller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalSystem.Controllers
+{
+    public class HospitalControlller : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
